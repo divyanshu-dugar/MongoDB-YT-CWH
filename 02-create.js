5d01891b46e79762471f5d0f52f0d24971b65c7d
Divyanshu Dugar <divyu2004@gmail.com>
2024-04-27 15:50:06 -0400

    added read commands 1.1

--- 02-create.js
+++ 02-create.js
@@ -3,13 +3,13 @@
 // Inserting one document/row to the inventory table/collection
 db.inventory.insertOne(
     { item: "canvas", qty: 100, tags: ["cotton"], size: { h: 28, w: 35.5, uom: "cm" } }
-)
+ )
 
 // Inserting multiple documents/rows to the inventory table/collection
-db.inventory.insertMany(
-[{ item: "canvas1", qty: 100, tags: ["cotton"], size: { h: 28, w: 35.5, uom: "cm" } },
-{ item: "canvas2", qty: 200, tags: ["silk"], size: { h: 38, w: 35.5, uom: "cm" } },
-{ item: "canvas3", qty: 300, tags: ["zinc"], size: { h: 8, w: 35.5, uom: "cm" } },
-{ item: "canvas4", qty: 400, tags: ["alu"], size: { h: 28, w: 35.5, uom: "cm" } },
-{ item: "canvas5", qty: 500, tags: ["cotton"], size: { h: 28, w: 35.5, uom: "cm" } }]
-)
+db.inventory.insertMany([
+    { "item": "journal", "qty": 25, "size": { "h": 14, "w": 21, "uom": "cm" }, "status": "A" },
+    { "item": "notebook", "qty": 50, "size": { "h": 8.5, "w": 11, "uom": "in" }, "status": "A" },
+    { "item": "paper", "qty": 100, "size": { "h": 8.5, "w": 11, "uom": "in" }, "status": "D" },
+    { "item": "planner", "qty": 75, "size": { "h": 22.85, "w": 30, "uom": "cm" }, "status": "D" },
+    { "item": "postcard", "qty": 45, "size": { "h": 10, "w": 15.25, "uom": "cm" }, "status": "A" }
+])
--- 03-read.js
+++ 03-read.js
@@ -1,8 +1,12 @@
 // Read - 
 
 // Fetch all documents
-db.inventory.find({});
+db.inventory.find({}); // ----> SELECT * FROM inventory
 
-db.inventory.find({qty:400});
+db.inventory.find( { status: "D" } ); // ----> SELECT * FROM inventory WHERE status = "D"
 
-db.inventory.find({tags:{$in: ["cotton","zinc"]}});
+db.inventory.find( { status: { $in: [ "A", "D" ] } } ); // ----> SELECT * FROM inventory WHERE status in ("A", "D")
+
+db.inventory.find( { status: "A", qty: { $lt: 30 } } ); // ----> SELECT * FROM inventory WHERE status = "A" AND qty < 30
+
+db.inventory.find( { $or: [ { status: "A" }, { qty: { $lt: 30 } } ] } ) // ----> SELECT * FROM inventory WHERE status = "A" OR qty < 30
